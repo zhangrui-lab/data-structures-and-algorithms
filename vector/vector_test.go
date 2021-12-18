1c647f449a7b6e9107606a5b04d18c614f715926
luoluoluoya <18684085840@163.com>
2021-12-18 09:05:11 +0800

    补充vector测试用例

--- vector/vector_test.go
+++ vector/vector_test.go
@@ -14,7 +14,7 @@ func TestVector_New(t *testing.T) {
 }
 
 func TestVector_Insert(t *testing.T) {
-	v1, v2, v3, v4 := types.Int(10), types.Int(5), types.Int(20), types.Int(40)
+	v1, v2 := types.Int(10), types.Int(5)
 	vec := New(3)
 	assert.Equal(t, vec.insert(0, v1), true, "insert != true")
 	assert.Equal(t, vec.insert(vec.Size()+1, v2), false, "insert != fasle")
@@ -34,31 +34,35 @@ func TestVector_Insert(t *testing.T) {
 	v, ok = vec.Back()
 	assert.Equal(t, v, v1, "vec[0] != 10")
 	assert.Equal(t, ok, nil, "err != nil")
+}
 
-	vec.Push(v3)
-	v, ok = vec.Pop()
-	assert.Equal(t, v, v3, "vec[0] != 20")
-	assert.Equal(t, ok, nil, "err != nil")
+func TestVector_Copy_Clear(t *testing.T) {
+	v1, v2, v3, v4 := types.Int(10), types.Int(20), types.Int(30), types.Int(40)
+	vec1 := CopySlice(v1, v2, v3, v4)
+	vec2 := Copy(vec1)
+	assert.Equal(t, vec2.Size(), 4, "size != 4")
+	assert.Equal(t, vec2.Capacity(), 4, "cap != 4")
+	vec2.Clear()
+	assert.Equal(t, vec2.Size(), 0, "size != 4")
+	assert.Equal(t, vec2.Capacity(), 4, "cap != 4")
+}
 
-	vec.Push(v2)
-	vec.Push(v3)
+func TestVector_Remove(t *testing.T) {
+	v1, v2, v3, v4 := types.Int(10), types.Int(20), types.Int(30), types.Int(40)
+	vec := CopySlice(v1, v2, v3, v4)
+	_, err := vec.Remove(vec.Size())
+	assert.NotEqual(t, err, nil, "err == nil")
 
-	vec.Assign(vec.Size()-1, v4)
-	v, _ = vec.At(vec.Size() - 1)
-	assert.Equal(t, v, v4, "vec.last != 40")
-
-	vec.RemoveRange(0, 2)
-	assert.Equal(t, vec.Size(), 1, "size != 1")
-
-	vec.Clear()
-	assert.Equal(t, vec.Size(), 0, "size != 0")
+	e, err := vec.Remove(0)
+	assert.Equal(t, e, v1, "e != 10")
+	assert.Equal(t, err, nil, "err != nil")
 }
 
 func TestVector_Find(t *testing.T) {
 	v1, v2, v3, v4 := types.Int(10), types.Int(20), types.Int(30), types.Int(40)
 	vec := CopySlice(v1, v2, v3, v4)
 	assert.Equal(t, vec.Size(), 4, "size != 4")
-	assert.Equal(t, vec.Capacity(), 4<<1, "cap != 6")
+	assert.Equal(t, vec.Capacity(), 4, "cap != 6")
 
 	r := vec.Find(v1)
 	assert.Equal(t, r, 0, "r != 0")
